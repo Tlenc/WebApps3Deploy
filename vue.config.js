e9dc36d8c224e5fd9e5bfabb7ca241b16de6be1c
Tlenc <tlenciauskas@gmail.com>
2022-03-19 01:38:22 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+
+const path = require("path");
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/WebApps3Deploy/'
-    : '/'
-}
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src", "components"),
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/WebApps3Deploy-form/" : "/",
+};
